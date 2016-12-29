a9666838bf58c84ae3678c51835813c97584b915
xinlingforever <xinlingforever99@gmail.com>
2016-12-29 19:59:23 +0800

    modify config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,25 @@
- var config = {
-   entry: {
-      a: ['./main.js']
-   },
-	
-   output: {
-      path:'./build',
-      filename: 'index.js',
-   },
-	
-   devServer: {
-      inline: true,
-      port: 7777
-   },
-	
-   module: {
-      loaders: [ {
-         test: /\.jsx?$/,
-         exclude: /node_modules/,
-         loader: 'babel',
-			
-         query: {
-            presets: ['es2015', 'react', 'stage-0']
-         }
-      }]
-   }
-	
-}
+var path = require('path');
 
-module.exports = config;
+module.exports = {
+    entry: ['webpack/hot/dev-server', path.resolve(__dirname, './main.js')],
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'bundle.js',
+    },
+
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loader: 'babel'
+        }, {
+            test: /\.css$/,
+            loader: 'style!css'
+        }, {
+            test: /\.less$/,
+            loader: 'style!css!less'
+        },{
+            test: /\.(png|jpg)$/,
+            loader: 'url?limit=25000'
+        }]
+    }
+};
